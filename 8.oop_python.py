56b6f4cd2c166657229a9f3dac9f40bf149afd2e
protyush1999 <protyushn@gmail.com>
2022-02-03 00:12:32 +0530

    files added

---
+++ 8.oop_python.py
@@ -0,0 +1,12 @@
+'''
+Class ==> A class is a blueprint for creating objects.
+Object ==> An object is an instantiation of a class.
+           When class is defined, a template(info) is defined.
+           Memory is allocated only after object instantiation.
+'''
+
+# Class have attributes and methods to define the class
+
+
+
+
